bb97ff01b94cda4c532326175f90babb8d990f91
Lahari Bandaru <lb825@cornell.edu>
2026-07-06 21:06:44 -0400

    replaced geoserver link with secure link

--- src/resources/links.js
+++ src/resources/links.js
@@ -1,7 +1,7 @@
 const links = {
     "standardMap": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     "counties": "https://crop-rotation-tool-lahari.s3.us-east-2.amazonaws.com/georgia-counties2.json",
-    "geoserver":"http://68.109.131.39:8080",
+    "geoserver":"https://smarsgroup.org/",
     "rotationLayer":"smarsgroup:ga_rotation",
     "renderServer":"https://crop-rotation-server.onrender.com"
 }
